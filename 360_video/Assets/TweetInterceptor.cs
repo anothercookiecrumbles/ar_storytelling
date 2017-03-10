002f9b8d36a6f6918f2d2c27c46b893591997c39
anothercookiecrumbles <priyanjana@mac.com>
2017-03-10 00:39:49 -0500

    Adds 360

---
+++ 360_video/Assets/TweetInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TweetInterceptor : MonoBehaviour {
+
+	object lockref = new object();
+
+	volatile Queue<string> tweets = new Queue<string>();
+	volatile Queue<string> usernames = new Queue<string>();
+
+	Text[] texts = new Text[5];
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] gos = GameObject.FindGameObjectsWithTag ("Tweet");
+		for (int i = 0; i < 5; i++) {
+			texts[i] = gos[i].GetComponent<Text>();
+		}
+
+		IEnumerator coroutine = TweetUpdateCoroutine ();
+		StartCoroutine (coroutine);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void UpdateTweetsInDisplay() {
+		// Both lists should be small so this isn't end-x§of-the-world bad. 
+		lock (lockref) {
+			Queue<string> tweets_ro = new Queue<string> (tweets);
+			Queue<string> usernames_ro = new Queue<string> (usernames);
+			for (int i = 0; i < 5; i++) {
+				Debug.Log (tweets_ro.Dequeue());
+				texts [i].text = usernames_ro.Dequeue() + ": " + tweets_ro.Dequeue();
+			}
+			Debug.Log("----");
+			tweets = tweets_ro;
+			usernames = usernames_ro;
+		}
+
+	}
+
+	// Adds tweets to the collection which will then be overlayed in some kind of an intelligent manner
+	public void UpdateTweetsToDisplay(string username, string tweet) {
+		lock (lockref) {
+			tweets.Enqueue (tweet);
+			usernames.Enqueue (username);
+		}
+	}
+
+	IEnumerator TweetUpdateCoroutine() {
+		for(;;) {
+			UpdateTweetsInDisplay();
+			// This is far from optimal but it's a good place to start, maybe? 
+			yield return new WaitForSeconds(10.0f);
+		}
+	}
+}
---
+++ 360_video/Assets/TwitterTalker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterTalker : MonoBehaviour {
+
+	public TweetInterceptor ti; 
+
+	// Use this for initialization
+	void Start () {
+		ti.UpdateTweetsToDisplay ("winter_is_coming", "He's on FIRE!!!! #CHIvsCLE");
+	
+		ti.UpdateTweetsToDisplay ("acookiecrumbles", "This is the most boring game ever #CHIvsCLE");
+		ti.UpdateTweetsToDisplay ("winter_is_coming", "YES!!!! #CHIvsCLE");
+		ti.UpdateTweetsToDisplay ("boo_hoo", "Striking");
+		ti.UpdateTweetsToDisplay ("smod4real", "The final seal of the apocalypse has been opened. #Cubs #WorldSeries");
+		ti.UpdateTweetsToDisplay ("AdamSerwer", "The last time the Cubs won the World Series, the Warriors had not yet blown a 3-1 lead in the NBA Final");
+		ti.UpdateTweetsToDisplay ("Reds", "¯\\_(ツ)_/¯");
+		ti.UpdateTweetsToDisplay ("FauxPelini", ".@Pontifex are you serious with the rain right now");
+		ti.UpdateTweetsToDisplay("mrsanniemac", "Haven't seen this many stressed out white dudes since the Ashley Madison hack.");
+		ti.UpdateTweetsToDisplay ("unknown", "Hey dude, big favor... I'm at the park, but PLEASE log into my Twitter and delete all my 3-1 memes from June.");
+		ti.UpdateTweetsToDisplay("SBNation", "THE 3-1 TORCH HAS BEEN PASSED");
+		ti.UpdateTweetsToDisplay ("ByTimReynolds", "This might not have been a great idea.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ti.UpdateTweetsToDisplay ("acookiecrumbles", "This is the most boring game ever #CHIvsCLE");
+		ti.UpdateTweetsToDisplay ("winter_is_coming", "YES!!!! #CHIvsCLE");
+		ti.UpdateTweetsToDisplay ("boo_hoo", "Striking");
+		ti.UpdateTweetsToDisplay ("smod4real", "The final seal of the apocalypse has been opened. #Cubs #WorldSeries");
+		ti.UpdateTweetsToDisplay ("AdamSerwer", "The last time the Cubs won the World Series, the Warriors had not yet blown a 3-1 lead in the NBA Final");
+		ti.UpdateTweetsToDisplay ("Reds", "¯\\_(ツ)_/¯");
+		ti.UpdateTweetsToDisplay ("FauxPelini", ".@Pontifex are you serious with the rain right now");
+		ti.UpdateTweetsToDisplay("mrsanniemac", "Haven't seen this many stressed out white dudes since the Ashley Madison hack.");
+		ti.UpdateTweetsToDisplay ("unknown", "Hey dude, big favor... I'm at the park, but PLEASE log into my Twitter and delete all my 3-1 memes from June.");
+		ti.UpdateTweetsToDisplay("SBNation", "THE 3-1 TORCH HAS BEEN PASSED");
+		ti.UpdateTweetsToDisplay ("ByTimReynolds", "This might not have been a great idea.");
+	}
+}
